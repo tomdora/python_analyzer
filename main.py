2ab95691915f53219c54735329bc90bf1287cc5c
Thomas Fiorilla <engineertf@gmail.com>
2021-03-05 22:18:49 -0500

    Intiial Upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+import string
+import tokenizer
+
+# Function to recursively look through directories
+def dirTravel(directory):
+	
+	return
+
+def main():
+	token = tokenizer.tokenize("Beep boop bop")
+	tokenizer.printList(token)
+	
+	return
+	
+if __name__ == "__main__":
+	main()
